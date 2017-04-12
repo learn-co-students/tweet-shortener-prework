8c0fe96beafbdf3cdf3e90c237d951b06b63dc40
Mike Luo <zluo@alumni.berklee.edu>
2017-04-12 20:47:29 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,43 @@
-# Write your code here.
+def word_substituter(tweet)
+
+  dictionary = {
+    "hello" => "hi",
+       "to" => "2",
+      "two" => "2",
+      "too" => "2",
+      "for" => "4",
+     "four" => "4",
+       "be" => "b",
+      "you" => "u",
+       "at" => "@",
+      "and" => "&"
+  }
+
+  tweet_arr = tweet.split(" ")
+  new_tweet = Array.new
+
+  tweet_arr.each do |word|
+    if dictionary.keys.include?(word.downcase)
+      new_tweet << dictionary[word.downcase]
+    else
+      new_tweet << word
+    end
+  end
+  new_tweet.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each { |tweet| puts word_substituter(tweet) }
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet[0..139]
+end
