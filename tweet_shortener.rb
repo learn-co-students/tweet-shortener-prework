a1425eaf3fb7ff0842f22552ac53c243558533b1
Jason  Rosso <jasonrosso@gmail.com>
2017-05-16 13:56:07 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -17,12 +17,36 @@ def word_substituter(tweet)
   new_tweet=[]
   dictionary
   tweet.split(" ").map do |word|
-    if dictionary[word]!=nil
-     word = dictionary[word]
+    if dictionary[word.downcase]!=nil
+     word = dictionary[word.downcase]
       new_tweet << word
     else
       new_tweet << word
     end
   end
   return new_tweet.join(" ")
-end 
+end
+
+def bulk_tweet_shortener(array_of_tweets)
+  array_of_tweets.collect do |tweet|
+   puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length>140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+substituted_tweet = word_substituter(tweet)
+
+  if substituted_tweet.length>140
+    return substituted_tweet[0..136]+"..."
+  else
+    return substituted_tweet
+  end
+end
