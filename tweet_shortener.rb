aa3acb217de3f198d3dd979ee270efbc26f02555
Jason  Rosso <jasonrosso@gmail.com>
2017-05-16 13:17:41 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,28 @@
-# Write your code here.
+def dictionary
+list ={
+  "hello" => 'hi',
+    "to" => '2',
+  "two" => '2',
+  "too" => '2',
+  "for" => '4',
+  "four" => '4',
+  "be" =>'b',
+  "you" => 'u',
+  "at" => "@",
+  "and"=>"&"
+}
+end
+
+def word_substituter(tweet)
+  new_tweet=[]
+  dictionary
+  tweet.split(" ").map do |word|
+    if dictionary[word]!=nil
+     word = dictionary[word]
+      new_tweet << word
+    else
+      new_tweet << word
+    end
+  end
+  return new_tweet.join(" ")
+end 
