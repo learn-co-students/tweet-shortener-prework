69f57af02d8b0bc8f7b02a2c7d7634a51c8a1aa4
c0nniewang <connie.wang303@gmail.com>
2017-10-05 14:18:37 -0500

    Done

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,63 @@
-# Write your code here.
+def dictionary
+	dictionary = {
+	"hello" => 'hi',
+	"to" => "2",
+	"two" => "2",
+	"too" => "2",
+	"for" => "4",
+	"four" => "4",
+	"be" => "b",
+	"you" => "u",
+	"at" => "@",
+	"and" => "&"
+	}
+end
+
+def word_substituter(string)
+	newstr = []
+	string.split(" ").each do |word|
+		if dictionary[word.downcase] != nil
+			newstr << dictionary[word.downcase]
+		else
+			newstr << word
+		end
+	end
+	newstr.join(" ")
+end
+
+def bulk_tweet_shortener(array)
+	array.each do |tweets|
+		puts word_substituter(tweets)
+	end
+end
+
+def selective_tweet_shortener(tweet)
+	if tweet.length > 140
+		word_substituter(tweet)
+	else
+		return tweet
+	end
+end
+
+def shortened_tweet_truncator(tweet)
+	if tweet.length > 140
+		new_tweet = word_substituter(tweet)
+		if new_tweet.length > 140
+			return new_tweet[0..136] + "..."
+		else
+			return new_tweet
+		end
+	else
+		return tweet
+	end
+end
+
+
+
+
+
+
+
+
+
+
