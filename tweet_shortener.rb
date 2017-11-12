16d2767015043bc70d6c027b5a31157f941aa21a
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-12 00:03:24 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -15,14 +15,19 @@ def dictionary
 end
 
 def word_substituter(tweet)
-  string_array = []
   string_array = tweet.split(' ')
   string_array.each_with_index do |string, index|
     dictionary.each do |word, abbrev|
-      if string.include?(word)
+      if string == word
         string_array[index] = abbrev
       end
     end
   end
   string_array.join(' ')
+end
+
+def bulk_tweet_shortener(array)
+  array.each do |tweet|
+    word_substituter(tweet)
+  end
 end
