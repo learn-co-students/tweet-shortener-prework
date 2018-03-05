402a17ab8f471c1e830948be457bb5349f2da404
Frank H Mcmanus <frank@thefrankharvey.com>
2018-03-05 02:15:54 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,45 @@
-# Write your code here.
+require 'pry'
+
+def dictionary
+{
+"hello" => 'hi',
+"to" => '2',
+"two" => '2',
+"too" => '2',
+"for" => '4',
+"four" => '4',
+"be" => 'b',
+"you" => 'u',
+"at" => '@',
+"and" => '&'
+}
+end
+
+def word_substituter(tweet)
+  tweet.split.collect { |word| dictionary.keys.include?(word.downcase) ?
+    word = dictionary[word.downcase] : word }.join(' ')
+      end
+
+def bulk_tweet_shortener(tweets)
+  tweets.collect { |tweet| puts word_substituter(tweet)}
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  elsif tweet.length <= 140
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+truncated_tweet = ""
+  if tweet.length < 140
+    truncated_tweet = tweet
+  elsif selective_tweet_shortener(tweet).length > 140
+    truncated_tweet << selective_tweet_shortener(tweet)[0...137] + "..."
+  else
+    truncated_tweet = selective_tweet_shortener(tweet)
+  end
+  truncated_tweet
+end
