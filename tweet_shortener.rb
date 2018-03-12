0b80f5969ee556269cba928e3dc5d2d02ece71bc
Yong-Nicholas Kim <yongnicholaskim@gmail.com>
2018-03-12 17:47:36 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,21 +1,23 @@
 # Write your code here.
 def dictionary
   dict = {
-    "hello"=> 'hi',
-    "to, two, too"=> '2',
-    "for, four"=> '4',
-    'be'=> 'b',
-    'you'=> 'u',
-    "at"=> "@",
-    "and"=> "&"
-  }
+    "too" => "2",
+    "to" => "2",
+    "two" =>"2",
+    "four" => "4",
+    "for" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+}
 end
 
 def word_substituter(str)
   strArr = str.split
   strArr.each do |word| 
     dictionary().any? do |k,v| 
-      if k.include? word.gsub(/(\W|\d)/, "").downcase
+      if k == word.gsub(/(\W|\d)/, "").downcase
         strArr[strArr.index(word)] = dictionary()[k]
       end
     end
@@ -39,7 +41,7 @@ end
 
 def shortened_tweet_truncator(str)
   if word_substituter(str).length > 140
-    "#{word_substituter(str)[0..140]}..."
+    "#{word_substituter(str)[0..136]}..."
   else 
     str
   end
