66b63d32274f4a6a52f2d9bc56ca61e7b3cc0c04
travisdock@gmail.com <travisdock@gmail.com>
2018-04-07 16:38:54 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,56 @@
-# Write your code here.
+def word_substituter(tweet)
+  subs = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "For" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and"=> "&"
+  }
+
+  array = tweet.split
+  new_array = []
+
+  array.collect do |word|
+    if subs.include?(word)
+      new_array.push(subs[word])
+    else
+      new_array.push(word)
+    end
+  end
+
+  array = new_array.join(" ")
+
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    tweet = word_substituter(tweet)
+    if tweet.length > 140
+      tweet = tweet[0..136] + "..."
+    else
+      tweet
+    end
+  else
+    tweet
+  end
+end
