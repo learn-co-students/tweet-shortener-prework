05acf0a0a22ca7403fca9c235c0af8813f78dfdc
QMaximillian <quinnlashinsky@gmail.com>
2018-04-21 14:16:30 -0400

    Done.

--- tweet_shortener_practice.rb
+++ tweet_shortener_practice.rb
@@ -1,22 +1,19 @@
-require 'pry'
+def dictionary = {
+	"hello": "hi",
+	"to": "2",
+	"two": "2",
+	"too": "2",
+	"for": "4",
+	"four": "4",
+	"be": "b",
+	"you": "u",
+	"at": "@",
+	"and": "&"
+}
 
-def dictionary
-	dictionary = {
-		"hello" => "hi",
-		"to" => "2",
-		"two" => "2",
-		"too" => "2", 
-		"for" => "4" ,
-		"four" => "4",
-		"be" => "b",
-		"you" => "u",
-		"at" => "@",
-		"and" => "&"
-	}
-end
 def word_substituter(tweet)
-
 	tweet_array = []
+
 	tweet.split(" ").each do |word|
 		if dictionary[word.downcase]
 			tweet_array << dictionary[word.downcase]
@@ -24,16 +21,14 @@ def word_substituter(tweet)
 			tweet_array << word
 		end
 	end
-
 	tweet_array.join(" ")
-
 end
 
 def bulk_tweet_shortener(tweet_array)
-	tweet_array.collect do |word|
+	tweet_array.each do |word|
 		puts word_substituter(word)
 	end
-
+	tweet_array
 end
 
 def selective_tweet_shortener(tweet)
@@ -46,8 +41,10 @@ end
 
 def shortened_tweet_truncator(tweet)
 	if selective_tweet_shortener(tweet).length > 140
-		selective_tweet_shortener(tweet)[0..139] 
+		selective_tweet_shortener(tweet)[0..139]
 	else
 		selective_tweet_shortener(tweet)
 	end
 end
+
+
