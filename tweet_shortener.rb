da738ab49b907f486ebb1ace30b6897b468351e4
mark sauer-utley <mark.utley.713@gmail.com>
2018-04-24 16:42:35 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,39 @@
-# Write your code here.
+def dictionary
+  word_hash = {
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split(" ").map do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word = word
+    end
+  end.join(" ")
+end
+
+def bulk_tweet_shortener(tweet_arr)
+  tweet_arr.each do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  tweet.length > 140 ? word_substituter(tweet) : tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet.length > 140 ? new_tweet = word_substituter(tweet) : new_tweet = tweet
+  new_tweet.length > 140 ? new_tweet = new_tweet[0..136] + "..." : new_tweet = new_tweet
+  new_tweet
+end
