e23e4d0359d151b3465a417270dcc9161e0f905e
github username <github email address>
2018-05-23 11:13:47 -0700

    dictionary and word_substituter

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def dictionary
+  replacements = {
+    :hello => "hi",
+    :too => "2",
+    :two => "2",
+    :to => "2",
+    :four => "4",
+    :for => "4",
+    :be => "b",
+    :you => "u",
+    :at => "@",
+    :and => "&"}
+end
+
+def word_substituter(tweet)
+  newarr = []
+  tweet.split.each do |word|
+    if dictionary.keys.include?(word.downcase)
+      newarr.push << dictionary[word.downcase]
+    else
+      newarr << word
+    end
+  end
+  newarr.join(" ")
+end
