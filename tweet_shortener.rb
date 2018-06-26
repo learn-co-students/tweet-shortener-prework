5c57cdf25a6cc93de0bf187b58d07e6c08dad0dc
Bryan Teng <bteng1226@gmail.com>
2018-06-26 05:40:03 +0000

    automatically backed up by learn

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,24 @@
 # Write your code here.
-def tweet_shorter(string)
-  
-
-
+def tweet_shortener(string)
+  words = string.split(" ")
+  dictionary = {
+    :hi => "hello",
+    :"2" => ["to", "two", "too"],
+    :"4" => ["for", "four"],
+    :b => "be",
+    :u => "you",
+    :"@" => "at",
+  }
+  new = []
+  index = 0
+  while index < words.length
+    x = words[index]
+    if dictionary.values.flatten.include?(x) 
+    new.push((dictionary.key(x).to_s))
+    else
+    new.push(x)
+    end
+    index+=1
+  end
+  new.join(" ")
 end
-
