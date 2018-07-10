8ed13050b18268ba77c8932a8b43a42b7fe63794
Malcolm Leake <echelon1089@gmail.com>
2018-07-10 04:38:58 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
 # Write your code here.
+require'pry'
+
+def dictionary 
+substitutes = { 
+  "hello" => "hi"
+  "to" => '2'
+  "two" => "2"
+  "too" => "2"
+  "for" => "4"
+  "four" => "4"
+  "be" => "b"
+  "you" => "u"
+  "at" => "@"
+  "and" => "&"
+}
+end
+  
+  
+  
+def word_substituter(tweet)
+  tweet.split(" ").map do |word|
+    if dictionary[word]   
+
+end
