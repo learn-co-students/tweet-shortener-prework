7c1ea7f4668de6afb69e36b6679147a84d7167a1
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-13 20:25:02 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,23 @@
-# Write your code here.
+def dictionary
+  tweet_shortener = {"hello": "hi", "to": "2", "two": "2", "too": "2", "for": "4", "four": "4", "be": "b", "you": "u", "at": "@", "and": "&"}
+end 
+
+
+def word_substituter(tweet)
+  
+end
+
+
+def bulk_tweet_shortener(array_of_tweets)
+  
+end 
+
+
+def selective_tweet_shortener(tweet)
+  
+end 
+
+
+def shortened_tweet_truncator(tweet)
+  
+end 
