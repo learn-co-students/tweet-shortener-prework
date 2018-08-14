4f5f6ba187f26986ebba70618f39d07ce54795c5
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-14 15:12:22 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -22,7 +22,7 @@ def word_substituter(tweet)
   new_tweet = new_tweet_words.join(" ")
   
   #But new_tweet is nothing but a long string of empty space (as is new_tweet_words.join(" ")).  So that means that the collect method that I'm running is turning tweet_words, an array of the original words, into new_tweet_words, an array of empty strings...  
-  #AND I just ran learn after having temporarily set new_tweet equal to simply new_tweet_words.join, without the (" ").  And it returned  ""  , a COMPLETELY empty string that didn't even contain a single space. So, having returned the code back to its pre-tweaked state, I imagine that each original word is being converted into  ""  .
+  #AND I just ran learn after having temporarily set new_tweet equal to simply new_tweet_words.join, without the (" "); and it returned  ""  , a COMPLETELY empty string that didn't even contain a single space. So, having returned the code back to its pre-tweaked state, I imagine that each original word is being converted into  ""  .
 end
 
 
