a52b3d5f85b9fb82726944ef3bcba93b71af2681
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-14 05:10:56 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,7 @@
+require 'pry'
+
 def dictionary
-  {"hello": "hi", "to": "2", "two": "2", "too": "2", "for": "4", "four": "4", "be": "b", "you": "u", "at": "@", "and": "&"}
+  dictionary_hash = {"hello": "hi", "to": "2", "two": "2", "too": "2", "for": "4", "four": "4", "be": "b", "you": "u", "at": "@", "and": "&"}
   
 end 
 
@@ -9,19 +11,23 @@ def word_substituter(tweet)
   d_h_keys = dictionary_hash.keys 
   # d_h_keys is an array consisting of |dhk|'s
   
-  tweet_words = tweet.split("_") 
+  tweet_words = tweet.split(" ") 
+   
   # tweet_words is an array consisting of  |tword|'s
   
   tweet_words.collect do |tword|
+    
     if d_h_keys.include?(tword)
-  
-       tweet_words[tweet_words.index(tword)] = dictionary_hash[tword]
-       new_tweet_words = tweet_words
-    end
-     new_tweet = new_tweet_words.join(" ")
+      tweet_words[tweet_words.index(tword)] = dictionary_hash[tword]
+      tweet_words
+binding.pry
+    end 
+     tweet_words
+     
+    new_tweet = tweet_words.join(" ")
   end
-  new_tweet 
   
+ 
 end
 
 
