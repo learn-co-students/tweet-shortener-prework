3bfa5c8b4745768ce88d99e5c0b00b1c5c0030da
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-14 19:04:00 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -18,19 +18,19 @@ def word_substituter(tweet)
   new_tweet_words = tweet_words.collect do |tword|
     if dictionary.keys.include?(tword)
     #At this point, dictionary.keys does indeed yield an array of the keys in the hash in the above dictionary method.
-    
        tweet_words[tweet_words.index(tword)] = dictionary.keys[tword]
-
+ 
     end 
+   
     new_tweet_words
+    
   end
-  new_tweet = new_tweet_words.join(" ")
   
-  #But new_tweet is nothing but a long string of empty space (as is new_tweet_words.join(" ")).  So that means that the collect method that I'm running is turning tweet_words, an array of the original words, into new_tweet_words, an array of empty strings...  
+  new_tweet = new_tweet_words.join(" ")
+   #But new_tweet is nothing but a long string of empty space (as is new_tweet_words.join(" ")).  So that means that the collect method that I'm running is turning tweet_words, an array of the original words, into new_tweet_words, an array of empty strings...  
   #AND I just ran learn after having temporarily set new_tweet equal to simply new_tweet_words.join, without the (" "); and it returned  ""  , a COMPLETELY empty string that didn't even contain a single space. So, having returned the code back to its pre-tweaked state, I imagine that each original word is being converted into  ""  .
 end
-
-
+binding.pry 
 def bulk_tweet_shortener(array_of_tweets)
   
 end 
