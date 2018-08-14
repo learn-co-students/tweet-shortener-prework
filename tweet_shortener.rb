722fce3fe6069e8dd184563caf38bffe3a3a602a
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-14 18:12:56 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,22 +1,24 @@
 require 'pry'
 
 def dictionary
-  dictionary_hash = {"hello": "hi", "to": "2", "two": "2", "too": "2", "for": "4", "four": "4", "be": "b", "you": "u", "at": "@", "and": "&"}
-  
+  {"hello": "hi", "to": "2", "two": "2", "too": "2", "for": "4", "four": "4", "be": "b", "you": "u", "at": "@", "and": "&"}
 end 
 
 
+
+
 def word_substituter(tweet)
-  dictionary_hash = dictionary
-  d_h_keys = dictionary_hash.keys 
-  # d_h_keys is an array consisting of |dhk|'s
+   
+ 
   tweet_words = tweet.split(" ") 
    # tweet_words is an array consisting of  |tword|'s
    #So, it's all working properly at least through here.
   
   new_tweet_words = tweet_words.collect do |tword|
-    if d_h_keys.include?(tword)
-       tweet_words[tweet_words.index(tword)] = dictionary_hash[tword]
+    if dictionary.keys.include?(tword)
+    #At this point, dictionary.keys does indeed yield an array of the keys in the hash in the above dictionary method.
+       tweet_words[tweet_words.index(tword)] = dictionary.keys[tword]
+       
     end 
   end
   new_tweet = new_tweet_words.join(" ")
