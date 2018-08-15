acde98bab296426dcd2319554192aa83926ba1af
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-15 19:58:37 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,5 +1,3 @@
-require 'pry'
-
 def dictionary
   {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
 end 
