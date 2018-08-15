4200643f9167e1d48f0b66d9413c2131e969d75b
Omar Gonzalez <ogonzaleznyc@gmail.com>
2018-08-15 19:34:59 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,7 +1,7 @@
 require 'pry'
 
 def dictionary
-  {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "four" => "4", "For" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
+  {"hello" => "hi", "to" => "2", "two" => "2", "too" => "2", "for" => "4", "For" => "4", "four" => "4", "be" => "b", "you" => "u", "at" => "@", "and" => "&"}
 end 
 
 def word_substituter(tweet)
