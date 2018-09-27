58d15535798d74e3ec0c4043ec8c41a48c347f0e
Natasha Buck <natashabuck1@gmail.com>
2018-09-27 20:14:53 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,48 @@
-# Write your code here.
+def dictionary
+  dict = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&",
+  }
+end
+
+def word_substituter(tweet)
+  dict = dictionary
+  tweet = tweet.split
+  tweet.each_with_index { |word, idx|
+    dict.each { |long, short|
+      if word.downcase == long
+        tweet[idx] = dict[long]
+      end
+    }  
+  }
+  tweet.join(" ")
+end
+
+def bulk_tweet_shortener(tweets_arr)
+  tweets_arr.each { |tweet|
+    puts word_substituter(tweet)
+  }
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    tweet = word_substituter(tweet)
+  end
+  tweet
+end
+
+def shortened_tweet_truncator(tweet)
+  tweet = selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    tweet = tweet[1..140]
+  end
+  tweet
+end
