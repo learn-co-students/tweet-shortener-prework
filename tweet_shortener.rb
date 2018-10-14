7611e6208ad37a8657ab4485871b6e0d72530721
jbharwood <josephblakeharwood@gmail.com>
2018-10-14 13:21:59 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,59 @@
-# Write your code here.
+require "pry"
+
+def dictionary()
+  dictionary = {
+    # hello: 'hi',
+    # to: '2',
+    # two: '2',
+    # too: '2',
+    # for: '4',
+    # four: '4',
+    # be: 'b',
+    # you: 'u',
+    # at: "@",
+    # and: "&"
+    'hello' => 'hi',
+    'to' => '2',
+    'two' => '2',
+    'too' => '2',
+    'for' => '4',
+    'For' => '4',
+    'four' => '4',
+    'be' => 'b',
+    'you' => 'u',
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(string)
+  sentence = string.split(" ")
+  sentence.each_with_index do |word, index|
+    if dictionary.keys.include?(word)
+      sentence[index] = dictionary[word]
+    end
+  end
+  sentence.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.each_with_index do |tweet, index|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if tweet.length > 140
+    "#{tweet[0..136]}..."
+  else
+    tweet
+  end
+end
