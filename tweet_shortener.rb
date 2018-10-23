78b74884c7d66491730b65ff22d04527223b606f
Connor Callahan <connorwcallahan@gmail.com>
2018-10-23 17:41:01 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,20 @@
-# Write your code here.
+require 'pry'
+
+
+def dictionary
+  words_to_sub = { hello: "hi", be: "b", you: "u", at: "@", and: "@", for: "4", four: "4", to: "2", two: "2", too: "2", to: "2"}
+end 
+
+def word_substituter(tweet)
+  symbol_sub_array = dictionary.keys
+  string_sub_array = symbol_sub_array.collect { |x| x.to_s }
+  tweet_array = tweet.split(" ")
+  tweet_array.each do |word|
+    if word.include?("hello", "be", "you", "at", "and", "for", "four", "to", "two", "too")
+      tweet_array[word] = "z"
+      
+    end 
+  end
+end
+
+
