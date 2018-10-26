72389bcce0d9f019809729c951a494cf6dff3d87
demiansims <demiansims@gmail.com>
2018-10-26 18:30:58 -0400

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
 # Write your code here.
+require "pry"
+
+def dictionary
+# establish dictionary hash to be called later
+
+dictionary_hash = {"hello" => "hi", "to" => "2", "too" => "2", "two"=> "2",  "for" => "4", "four" => "4", "be" => "b",
+"you" => "u", "at" => "@", "and" => "&"}
+
+end
+
+
+def word_substituter(string_tweet)
+
+  array_tweet = string_tweet.split(" ")
+  shortened_word = array_tweet.collect do |i|
+    if dictionary.keys.include?(i)
+       dictionary[i]
+     else
+       i
+    end
+  end
+  shortened_word.join(" ")
+end
