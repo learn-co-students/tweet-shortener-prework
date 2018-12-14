1fc4b3de963ed80a5b075224747ed515bd474932
Jack Rotta <jackrotta12@gmail.com>
2018-12-14 20:27:32 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,25 @@
-# Write your code here.
+def dictionary
+  dict_hash = {
+    "hello" => "hi",
+    "to" =>  "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "@"
+  }
+end
+
+def word_substituter(string)
+  newS = string.split.collect do |word|
+    if dictionary.keys.include?(word.downcase)
+      word = dictionary[word.downcase]
+    else
+      word
+    end
+  end
+  newS.join(" ")
+end
