be96c8481f58205d8e6814f54e12a64ab04f2e8a
Alex Hare <alexanderhare14@gmail.com>
2019-04-18 17:30:30 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,49 @@
 # Write your code here.
+def dictionary
+  substitutes = {
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  }
+end
+
+def word_substituter(tweet)
+  array = tweet.split(" ")
+  array.map do |word|
+    dictionary.map do |key, value|
+      if word.downcase == key
+        word.replace(value)
+      end
+    end
+  end
+  array.join(" ")
+end
+
+def bulk_tweet_shortener(tweets)
+  tweets.map do |tweet|
+    puts word_substituter(tweet)
+  end
+end
+
+def selective_tweet_shortener(tweet)
+  if tweet.chars.length > 140
+    word_substituter(tweet)
+  else
+    tweet
+  end
+end
+
+def shortened_tweet_truncator(tweet)
+  if word_substituter(tweet).chars.length > 140
+    word_substituter(tweet).to_s[0..136] + "..."
+  else
+    word_substituter(tweet)
+  end
+end
