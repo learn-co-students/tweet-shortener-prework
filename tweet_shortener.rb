d20c16dff48e0cd4d442180e054b86894ff264d4
brinda behal <brindab50@gmail.com>
2019-05-24 03:19:46 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,1 +1,24 @@
-# Write your code here.
+def dictionary()
+  dictionary = {
+    :hello => ["hi"]
+    :to_two_too => ["2"]
+    :for_four => ["4"]
+    :be => ["b"]
+    :you => ["u"]
+    :at => ["@"]
+    :and => ["&"]
+  }
+end
+
+def word_substituter(tweet)
+  tweet.split
+  tweet.each do |tweet|
+    tweet == dictionary
+    if tweet = dictionary
+      return 
+    else 
+      
+    
+  
+end
+  
