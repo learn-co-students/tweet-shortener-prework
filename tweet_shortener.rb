7c0fd207a308818af2061028e052298aa57cb81c
brinda behal <brindab50@gmail.com>
2019-05-25 00:59:13 +0000

    Done.

--- tweet_shortener.rb
+++ tweet_shortener.rb
@@ -1,24 +1,58 @@
+require "pry"
 def dictionary()
   dictionary = {
-    :hello => ["hi"]
-    :to_two_too => ["2"]
-    :for_four => ["4"]
-    :be => ["b"]
-    :you => ["u"]
-    :at => ["@"]
-    :and => ["&"]
+    "hello" => "hi",
+    "to" => "2",
+    "two" => "2",
+    "too" => "2",
+    "for" => "4",
+    "four" => "4",
+    "be" => "b",
+    "you" => "u",
+    "at" => "@",
+    "and" => "&"
+  
   }
-end
+  end
 
 def word_substituter(tweet)
-  tweet.split
-  tweet.each do |tweet|
-    tweet == dictionary
-    if tweet = dictionary
-      return 
-    else 
-      
+
+  tweet.split.collect do |word|
     
-  
+   if dictionary.keys.include?(word.downcase)
+    
+       word = dictionary[word.downcase]
+    else
+      word
+    end
+  end.join(" ")
 end
   
+def bulk_tweet_shortener(tweet)
+    tweets.each do |tweet|
+    puts word_substituter(tweet)
+    
+  end
+end
+  
+  def selective_tweet_shortener(tweet)
+  
+      if tweet.length > 140
+        return word_substituter(tweet)
+      else
+        tweet[0..140]
+        #binding.pry
+
+        tweet
+    
+      end 
+    end
+    
+    def shortened_tweet_truncator(tweet)
+      tweets = word_substituter(tweet)
+      if tweets.length > 140
+        tweets[0...135] + ". . ."
+      else
+       tweets
+      end
+    end
